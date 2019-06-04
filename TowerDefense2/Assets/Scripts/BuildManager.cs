deea2683b66ba806e2d9cb85c1b0e4cec41adcda
tomimarkus991 <48998818+tomimarkus991@users.noreply.github.com>
2019-06-04 12:40:28 +0300

    Some new stuff

--- TowerDefense2/Assets/Scripts/BuildManager.cs
+++ TowerDefense2/Assets/Scripts/BuildManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BuildManager : MonoBehaviour
 {
@@ -19,9 +17,12 @@ public class BuildManager : MonoBehaviour
     public GameObject standardTurretPrefab;
     public GameObject missileLauncherPrefab;
 
+    public GameObject buildEffect;
+
     private TurretBlueprint turretToBuild;
 
     public bool CanBuild { get { return turretToBuild != null; } }
+    public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } }
 
     public void BuildTurretOn(Node node)
     {
@@ -33,6 +34,9 @@ public class BuildManager : MonoBehaviour
         PlayerStats.Money -= turretToBuild.cost;
         GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
         node.turret = turret;
+
+        GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
 
         Debug.Log("Turret build! Money left: " + PlayerStats.Money);
     }
--- TowerDefense2/Assets/Scripts/Node.cs
+++ TowerDefense2/Assets/Scripts/Node.cs
@@ -1,61 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour
 {
-public Color hoverColor;
-public Vector3 positionOffset;
+    public Color hoverColor;
+    public Color notEnoughMoneyColor;
+
+    public Vector3 positionOffset;
     [Header("Optional")]
-public GameObject turret; // kohe alguses on turret juba maas
+    public GameObject turret; // kohe alguses on turret juba maas
 
-private Renderer rend;
-private Color startColor;
+    private Renderer rend;
+    private Color startColor;
 
-BuildManager buildManager;
+    BuildManager buildManager;
 
-void Start()
-{
-    rend = GetComponent<Renderer>();
-    startColor = rend.material.color;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
 
-    buildManager = BuildManager.instance;
-}
+        buildManager = BuildManager.instance;
+    }
 
     public Vector3 GetBuildPosition()
     {
         return transform.position + positionOffset;
     }
-void OnMouseDown()
-{
-    if (EventSystem.current.IsPointerOverGameObject())
-        return;
-    if (!buildManager.CanBuild)
+    void OnMouseDown()
     {
-        return;
-    }
-    if (turret != null) // kui ei saa turretit sinna ehitada
-    {
-        Debug.Log("Can't build there! - TODO: Display on screen.");
-        return;
-    }
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+        if (!buildManager.CanBuild)
+        {
+            return;
+        }
+        if (turret != null) // kui ei saa turretit sinna ehitada
+        {
+            Debug.Log("Can't build there! - TODO: Display on screen.");
+            return;
+        }
         buildManager.BuildTurretOn(this);
-}
-void OnMouseEnter()
-{
-    if (EventSystem.current.IsPointerOverGameObject())
-        return;
-    if (!buildManager.CanBuild)
-    {
-        return;
     }
-    rend.material.color = hoverColor;
-}
+    void OnMouseEnter()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
 
-void OnMouseExit()
-{
-    rend.material.color = startColor;
-}
+        if (!buildManager.CanBuild)
+        {
+            return;
+        }
 
+        if (buildManager.HasMoney)
+        {
+            rend.material.color = hoverColor;
+        }
+        else
+        {
+            rend.material.color = notEnoughMoneyColor;
+        }
+
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
 }
